456057b113e9e78308a3cd1df448286c62e314d3
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-04-03 15:25:19 +0300

    Add base strucutre

---
+++ client/src/components/RootLayout.js
@@ -0,0 +1,16 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+        <header>
+            <nav>
+                <NavLink to="/">Movies</NavLink>
+            </nav>
+        </header>
+        <main>
+            <Outlet/>
+        </main>
+    </div>
+  )
+}
