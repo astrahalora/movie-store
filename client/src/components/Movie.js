95700c7bd918426c73c08bd67fcf2f86bdac1225
Astrahalora <astrahalora@gmail.com>
2023-04-04 08:48:12 +0300

    Moved movie code to Movie

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,5 +1,11 @@
 export default function Movie({movie}) {
   return (
-    <div>Movie</div>
+    <div className='movie'>
+      <img src={movie.Poster} alt={movie.Title}/>
+      <h3>{movie.Title}</h3>
+      <p>$14.99</p>
+      <button className='add-remove-favorites'>+</button>
+      <button className='add-remove-cart'>Add to Cart</button>
+    </div>
   )
 }
