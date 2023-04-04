5c253dcd2731d4d8a0972596708888c039deb96a
Astrahalora <astrahalora@gmail.com>
2023-04-04 16:18:34 +0300

    Add new component

---
+++ client/src/components/FavoriteMovies.js
@@ -0,0 +1,5 @@
+export default function FavoriteMovies() {
+  return (
+    <div>FavoriteMovies</div>
+  )
+}
--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -6,16 +6,22 @@ export default function MovieDetails({movie, onClick}) {
           <img src={movie.Poster} alt={movie.Title}/>
         </div>
         <div className="item-2">
-          <h2>{movie.Title}</h2>
-          <p><b>Release Date:</b> {movie.Released}</p>
-          <p><b>Runtime:</b> {movie.Runtime}</p>
-          <p><b>Genres:</b> {movie.Genre}</p>
-          <p><b>Directors:</b> {movie.Director}</p>
-          <p><b>Writers:</b> {movie.Writer}</p>
-          <p><b>Actors:</b> {movie.Actors}</p>
-          <p><b>Plot:</b> {movie.Plot}</p>
-          <p><b>IMDb Rating:</b> {movie.imdbRating}</p>
-          <p><b>Awards:</b> {movie.Awards}</p>
+          <div className="main-details">
+            <h2>{movie.Title}</h2>
+            <p><b>Release Date:</b> {movie.Released}</p>
+            <p><b>Runtime:</b> {movie.Runtime}</p>
+            <p><b>Genres:</b> {movie.Genre}</p>
+            <p><b>Directors:</b> {movie.Director}</p>
+            <p><b>Writers:</b> {movie.Writer}</p>
+            <p><b>Actors:</b> {movie.Actors}</p>
+            <p><b>Plot:</b> {movie.Plot}</p>
+            <p><b>IMDb Rating:</b> {movie.imdbRating}</p>
+            <p><b>Awards:</b> {movie.Awards}</p>
+          </div>
+          <div className="add-remove">
+            <button className="add-remove-favs">+</button>
+            <button className="add-remove-cart">Add to Cart</button>
+          </div>
         </div>
       </div>
       <div className="details-2">
