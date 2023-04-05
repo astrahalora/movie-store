e050b3ffe68cab5e0e7eb6f475f909b29fa5dcbf
Astrahalora <astrahalora@gmail.com>
2023-04-05 14:11:54 +0300

    Add route for cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
 import Home from "./components/Home";
-import './App.scss';
 import FavoriteMovies from "./components/FavoriteMovies";
+import Cart from "./components/Cart";
+import './App.scss';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout/>}>
       <Route index element={<Home/>}/>
       <Route path="/favorites" element={<FavoriteMovies/>}/>
+      <Route path="/checkout" element={<Cart/>}/>
     </Route>
   )
 )
